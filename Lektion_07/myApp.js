6ff30d41744f84f7cd28448833af362fb9041929
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2020-02-03 13:51:13 +0100

    Lektion 7

--- Lektion_07/myApp.js
+++ Lektion_07/myApp.js
@@ -1,8 +1,10 @@
 // Skapa en Angular-modul
+// En modul är en container (behållare) 
 let myApp = angular.module("myApp" , []);
 
 // Skapa en controller
-myApp.controller("myCtrl", function ($scope) {
+// En controller som hanterar en person
+myApp.controller("personCtrl", function ($scope) {
 
   // Egenskaper
   $scope.firstName = "";
@@ -13,3 +15,45 @@ myApp.controller("myCtrl", function ($scope) {
     return $scope.firstName + ' ' + $scope.lastName;
   }
 });
+
+// En controller som hanterar en varukorgen
+myApp.controller("shopCtrl" , function($scope) {
+
+  // Egenskaper / Data / Variabler
+  $scope.qty = 1;
+  $scope.cost = 10;
+
+  // Metod
+  $scope.total = function () {
+    return $scope.qty * $scope.cost
+  }
+});
+
+// En controller som hanterar bilar
+myApp.controller("carCtrl", function($scope){
+
+  // Skapa en array som innehåller ett antal bilar
+  $scope.cars = [
+    {
+      Regnr : "ABC123",
+      Price : 50000,
+      Model : "Volvo V40"
+    },
+    {
+      Regnr : "ZYZ123",
+      Price : 70000,
+      Model : "Volvo XC60"
+    },
+    {
+      Regnr : "BVC123",
+      Price : 110000,
+      Model : "Volvo XC90"
+    },
+  ];
+}); 
+  
+
+
+
+
+
